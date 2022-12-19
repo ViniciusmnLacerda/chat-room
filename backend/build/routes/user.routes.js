5bb373be3ae6f945c8e4d73138e5ffeebd8421ad
Vinícius Lacerda <viniciuslmn@gmail.com>
2022-12-19 11:12:56 -0300

    fix middleware post /messages/chatId

--- backend/build/routes/user.routes.js
+++ backend/build/routes/user.routes.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const controllers_1 = require("../controllers");
 const middlewares_1 = require("../middlewares");
 const router = (0, express_1.Router)();
-router.get('/', controllers_1.usersController.getAll);
+router.get('/', middlewares_1.verifyJwt, controllers_1.usersController.getAll);
 router.get('/:email', middlewares_1.verifyJwt, controllers_1.usersController.getUser);
 router.post('/login', middlewares_1.loginMiddleware, controllers_1.usersController.login);
 router.post('/signup', middlewares_1.signupMiddleware, controllers_1.usersController.signup);
