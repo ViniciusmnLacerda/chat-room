95eadc2349d54cc3fd97699613fc60e9315e089e
Vinícius Lacerda <viniciuslmn@gmail.com>
2022-12-20 19:24:10 -0300

    fix seeders

--- backend/src/database/seeders/20221215145209-create-users.js
+++ backend/src/database/seeders/20221215145209-create-users.js
@@ -7,15 +7,15 @@ module.exports = {
       {
         username: 'j.castro',
         email: 'julia@email.com',
-        password: '$2b$10$nD5Gv4EpKhXSzQ9QWPR2T.oPnLsHm8XTz3az6kBiykaW2m7S23oi.',
+        password: '$2b$10$pv82gQ6lNRZ8W1ty91ljheq9.Um6gadvoZVg.qBQ4HtD4.hrNxDAa',
         name: 'Julia',
         last_name: 'Castro',
         image: 'https://images.unsplash.com/photo-1664575602554-2087b04935a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
       },
       {
         username: 'j.lopes',
-        email: 'joão@email.com',
-        password: '$2b$10$nD5Gv4EpKhXSzQ9QWPR2T.oPnLsHm8XTz3az6kBiykaW2m7S23oi.',
+        email: 'joao@email.com',
+        password: '$2b$10$pv82gQ6lNRZ8W1ty91ljheq9.Um6gadvoZVg.qBQ4HtD4.hrNxDAa',
         name: 'João',
         last_name: 'Lopes',
         image: 'https://images.unsplash.com/photo-1669383488518-3f367058d9db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=747&q=80'
@@ -23,7 +23,7 @@ module.exports = {
       {
         username: 'l.azevedo',
         email: 'lucas@email.com',
-        password: '$2b$10$nD5Gv4EpKhXSzQ9QWPR2T.oPnLsHm8XTz3az6kBiykaW2m7S23oi.',
+        password: '$2b$10$pv82gQ6lNRZ8W1ty91ljheq9.Um6gadvoZVg.qBQ4HtD4.hrNxDAa',
         name: 'Lucas',
         last_name: 'Azevedo',
         image: 'https://images.unsplash.com/photo-1670525975578-4051a7803c38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
@@ -31,7 +31,7 @@ module.exports = {
       {
         username: 'v.lacerda',
         email: 'vinicius@email.com',
-        password: '$2b$10$nD5Gv4EpKhXSzQ9QWPR2T.oPnLsHm8XTz3az6kBiykaW2m7S23oi.',
+        password: '$2b$10$pv82gQ6lNRZ8W1ty91ljheq9.Um6gadvoZVg.qBQ4HtD4.hrNxDAa',
         name: 'Vinicius',
         last_name: 'Lacerda',
         image: 'https://images.unsplash.com/photo-1669985587439-a2b96062c931?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
@@ -39,7 +39,7 @@ module.exports = {
       {
         username: 'm.bragança',
         email: 'marianne@email.com',
-        password: '$2b$10$nD5Gv4EpKhXSzQ9QWPR2T.oPnLsHm8XTz3az6kBiykaW2m7S23oi.',
+        password: '$2b$10$pv82gQ6lNRZ8W1ty91ljheq9.Um6gadvoZVg.qBQ4HtD4.hrNxDAa',
         name: 'Marianne',
         last_name: 'Bragança',
         image: 'https://images.unsplash.com/photo-1670439999166-888c4386d15a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
@@ -47,7 +47,7 @@ module.exports = {
       {
         username: 'm.zita',
         email: 'zita@email.com',
-        password: '$2b$10$nD5Gv4EpKhXSzQ9QWPR2T.oPnLsHm8XTz3az6kBiykaW2m7S23oi.',
+        password: '$2b$10$pv82gQ6lNRZ8W1ty91ljheq9.Um6gadvoZVg.qBQ4HtD4.hrNxDAa',
         name: 'Maria',
         last_name: 'Zita',
         image: 'https://images.unsplash.com/photo-1669993427100-221137cc7513?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=670&q=80'
